d21c00bbd2be43dfda7c52303c404ff92c9aae4f
Cereno Deng <cereno.deng@gmail.com>
2025-04-14 00:43:21 +0800

    fix: path is not defined

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@ import { withPayload } from '@payloadcms/next/withPayload'
 
 import redirects from './redirects.js'
 
+const path = require('path')
+
 const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
   ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
   : undefined || process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000'
